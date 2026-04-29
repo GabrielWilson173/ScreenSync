ff8d2981de3630320d40a209b56be51601618996
BradleyN <nathanbradley2k2@gmail.com>
2026-04-28 21:04:35 -0500

    Fix missing import in main.py and add it to be merged

--- Prototyping/main.py
+++ Prototyping/main.py
@@ -17,7 +17,7 @@ SECRET_KEY = "SECRET_KEY"
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], # For testing, in production use specific URL
+    allow_origins=["*"], 
     allow_methods=["*"],
     allow_headers=["*"],
 )
@@ -38,7 +38,7 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="Login")
 
 #Takes a given uuid and generates a signed JWT to return to the user
 def Create_Token(uuid: str):
-    #Tells the server the user and the expiration time
+    #Tells the server the user and the expiration time 
     payload = {
         "uuid": uuid,
         "exp": datetime.now(tz=timezone.utc) + timedelta(minutes=90)
@@ -52,10 +52,10 @@ def Get_Current_User_Uuid(token: str = Depends(oauth2_scheme)):
     try:
         payload = decode(token, SECRET_KEY, algorithms=["HS256"])
         user_uuid = payload.get("uuid")
-
+        
         if not isinstance(user_uuid, str):
             raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED,
+                status_code=status.HTTP_401_UNAUTHORIZED, 
                 detail="UUID missing in token"
             )
         return user_uuid
@@ -63,11 +63,11 @@ def Get_Current_User_Uuid(token: str = Depends(oauth2_scheme)):
         print(f"JWT Error Type: {type(e).__name__}")
         print(f"JWT Error Message: {e}")
         raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
+            status_code=status.HTTP_401_UNAUTHORIZED, 
             detail="Invalid or expired token"
         )
-
-#Register user is run an HTTP POST request is sent to http://localhost:8000/Register/
+    
+#Register user is run an HTTP POST request is sent to http://localhost:8000/Register/ 
 @app.post("/Register/")
 async def Register_User(user: User):
     try:
@@ -86,18 +86,18 @@ async def Register_User(user: User):
         #If a user can't be inserted into the SQL table, raise an exception
         #Gets sent back to the user as an HTTP message with status_code != 200
         raise HTTPException(
-            status_code=400,
+            status_code=400, 
             detail="Username already taken"
         )
 
-#Login is run an HTTP POST request is sent to http://localhost:8000/Login/
+#Login is run an HTTP POST request is sent to http://localhost:8000/Login/  
 @app.post("/Login/")
 async def Login(user: User):
     try:
         username = user.username
-        #Use the username to get the hashed and salted password of the user
+        #Use the username to get the hashed and salted password of the user 
         user_info = getUserInfo(username)
-
+        
         #Check that the entered password hashes to the same as the stored hash of the password
         if password_hasher.verify(user.password, user_info["Password"]):
             return Create_Token(user_info["uuid"])
@@ -105,7 +105,7 @@ async def Login(user: User):
             raise HTTPException(status_code=401, detail="Invalid username or password")
     except:
         raise HTTPException(status_code=401, detail="Invalid username or password")
-
+    
 @app.post("/Screentime/Add")
 async def Add_Screentime(screentime_data: ScreenTimeData, uuid = Depends(Get_Current_User_Uuid)):
     data = screentime_data.data
@@ -117,12 +117,7 @@ async def Add_Screentime(screentime_data: ScreenTimeData, uuid = Depends(Get_Cur
         raise Exception("could not get device number")
 
     for app_name, seconds in data.items():
-<<<<<<< HEAD
         insertScreenTime(uuid, device_num, app_name, seconds)
-=======
-        insertScreenTime(uuid, app_name, seconds)
-        credit_system.updateCredits(uuid, app_name, seconds)
->>>>>>> refs/remotes/origin/login-system-prototype
 
 @app.get("/Screentime/Get")
 async def Get_Screentime(uuid = Depends(Get_Current_User_Uuid)):
@@ -130,4 +125,5 @@ async def Get_Screentime(uuid = Depends(Get_Current_User_Uuid)):
 
 @app.get("/Credits/Get")
 async def Get_Credits(uuid = Depends(Get_Current_User_Uuid)):
+    #Test feature. We didn't finish this.
     return credit_system.getCredits(uuid)
